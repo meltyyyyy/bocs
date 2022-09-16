837a568c90e91e8d7b1ad25fea19b3530ebbe1b7
takeru.abe <takeru.abe@gaiax.com>
2022-09-16 15:51:25 +0900

    update bocs.py

---
+++ sblr/bhs.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-pastel')
+
+
+# Bayesian Horseshoe Sampler
+# References:
+# A simple sampler for the horseshoe estimator
+# https://arxiv.org/pdf/1508.03884.pdf
+def bhs(X, y, n_samples=1000, burnin=200):
+    n, p = X.shape
+    XtX = X.T @ X
+
+    beta = np.zeros((p, n_samples))
+    sigma2 = 1
+    lambda2 = np.random.uniform(size=p)
+    tau2 = 1
+    nu = np.ones(p)
+    xi = 1
+
+    # Run Gibbs Sampler
+    for i in range(n_samples + burnin):
+        Lambda_star = tau2 * np.diag(lambda2)
+        A = XtX + np.linalg.inv(Lambda_star)
+        A_inv = np.linalg.inv(A)
+        b = np.random.multivariate_normal(A_inv @ X.T @ y, sigma2 * A_inv)
+
+        # Sample sigma^2
+        e = y - np.dot(X, b)
+        shape = (n + p) / 2.
+        scale = np.dot(e.T, e) / 2. + np.sum(b**2 / lambda2) / tau2 / 2.
+        sigma2 = 1. / np.random.gamma(shape, 1. / scale)
+
+        # Sample lambda^2
+        scale = 1. / nu + b**2. / 2. / tau2 / sigma2
+        lambda2 = 1. / np.random.exponential(1. / scale)
+
+        # Sample tau^2
+        shape = (p + 1.) / 2.
+        scale = 1. / xi + np.sum(b**2. / lambda2) / 2. / sigma2
+        tau2 = 1. / np.random.gamma(shape, 1. / scale)
+
+        # Sample nu
+        scale = 1. + 1. / lambda2
+        nu = 1. / np.random.exponential(1. / scale)
+
+        # Sample xi
+        scale = 1. + 1. / tau2
+        xi = 1. / np.random.exponential(1. / scale)
+
+        if i >= burnin:
+            beta[:, i - burnin] = b
+
+    return beta
+
+
+if __name__ == '__main__':
--- sblr/horseshoe.py
+++ sblr/horseshoe.py
@@ -3,55 +3,36 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-pastel')
 
 
-# Bayesian Horseshoe Sampler
-# References:
-# A simple sampler for the horseshoe estimator
-# https://arxiv.org/pdf/1508.03884.pdf
-def bhs(X, y, n_samples=1000, burnin=200):
-    n, p = X.shape
-    XtX = X.T @ X
-
-    beta = np.zeros((p, n_samples))
-    sigma2 = 1
-    lambda2 = np.random.uniform(size=p)
-    tau2 = 1
-    nu = np.ones(p)
-    xi = 1
-
-    # Run Gibbs Sampler
-    for i in range(n_samples):
-        Lambda_star = tau2 * np.diag(lambda2)
-        A = XtX + np.linalg.inv(Lambda_star)
-        A_inv = np.linalg.inv(A)
-        b = np.random.multivariate_normal(A_inv @ X.T @ y, sigma2 * A_inv)
-
-        # Sample sigma^2
-        e = y - np.dot(X, b)
-        shape = (n + p) / 2.
-        scale = np.dot(e.T, e) / 2. + np.sum(b**2 / lambda2) / tau2 / 2.
-        sigma2 = 1. / np.random.gamma(shape, 1. / scale)
-
-        # Sample lambda^2
-        scale = 1. / nu + b**2. / 2. / tau2 / sigma2
-        lambda2 = 1. / np.random.exponential(1. / scale)
-
-        # Sample tau^2
-        shape = (p + 1.) / 2.
-        scale = 1. / xi + np.sum(b**2. / lambda2) / 2. / sigma2
-        tau2 = 1. / np.random.gamma(shape, 1. / scale)
-
-        # Sample nu
-        scale = 1. + 1. / lambda2
-        nu = 1. / np.random.exponential(1. / scale)
-
-        # Sample xi
-        scale = 1. + 1. / tau2
-        xi = 1. / np.random.exponential(1. / scale)
-
-        if iter > burnin:
-            beta[:, i] = b
-
-    return beta
+def horseshoe(tau=1.0):
+    k = np.random.beta(0.5, 0.5, size=100)
+    λ = np.sqrt(-1 + 1 / k)
+    scale = (tau ** 2) * (λ ** 2)
+    samples = np.random.normal(0, scale)
+    return samples
 
 
-if __name__ == '__main__':
+def plot(samples):
+    fig = plt.figure()
+    plt.hist(samples, bins=100)
+    plt.savefig('horseshoe.png')
+    plt.close()
+
+
+def main():
+    beta = np.random.beta(0.5, 0.5, size=1000000)
+    fig = plt.figure()
+    plt.hist(beta, bins=100)
+    plt.savefig('dist-k.png')
+    plt.close()
+
+    samples = horseshoe()
+    fig = plt.figure()
+    plt.hist(samples, bins=100)
+    plt.savefig('horseshoe.png')
+    plt.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
