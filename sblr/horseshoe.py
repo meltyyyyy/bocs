aa638ef5bc88e102656a4d99d2382936191e9fa7
takeru.abe <takeru.abe@gaiax.com>
2022-09-17 14:11:34 +0900

    update horseshoe.py

--- sblr/horseshoe.py
+++ sblr/horseshoe.py
@@ -3,50 +3,31 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-pastel')
 
 
-def horseshoe(tau=1.0):
+def horseshoe():
+    tau=1.0
     k = np.random.beta(0.5, 0.5, size=10000)
-    λ = np.sqrt(-1 + 1 / k)
+    λ = np.sqrt(-1 + 1 / k) / tau
     scale = (tau ** 2) * (λ ** 2)
     samples = np.random.normal(0, scale)
     return samples
 
 
 def main():
-    beta = np.random.beta(0.3, 0.3, size=100000)
-    fig, axes = plt.subplots(1, 3, figsize=(24, 8))
-    fig.suptitle(f"Beta ditribution", fontsize=18, fontweight='bold')
-    axes[0].set_title("Beta(0.3, 0.3)")
-    axes[0].hist(beta, bins=100)
-
     beta = np.random.beta(0.5, 0.5, size=100000)
-    axes[1].set_title("Beta(0.5, 0.5)")
-    axes[1].hist(beta, bins=100)
-
-    beta = np.random.beta(0.7, 0.7, size=100000)
-    axes[2].set_title("Beta(0.7, 0.7)")
-    axes[2].hist(beta, bins=100)
-
-    fig.tight_layout()
+    fig = plt.figure()
+    plt.title("Beta(0.5, 0.5)")
+    plt.hist(beta, bins=100)
     fig.savefig("dist-k.png")
     plt.close()
 
-    samples = horseshoe(tau=1.0)
-    fig, axes = plt.subplots(1, 3, figsize=(24, 8))
-    fig.suptitle(f"Horseshoe ditribution", fontsize=18, fontweight='bold')
-    axes[0].set_title("tau=1.0")
-    axes[0].hist(samples, bins=100)
-
-    samples = horseshoe(tau=100.0)
-    axes[1].set_title("tau=100")
-    axes[1].hist(samples, bins=100)
-
-    samples = horseshoe(tau=0.01)
-    axes[2].set_title("tau=0.01")
-    axes[2].hist(samples, bins=100)
-
-    fig.tight_layout()
+    samples = horseshoe()
+    fig = plt.figure()
+    plt.title("Horseshoe ditribution")
+    plt.hist(samples, bins=100)
     fig.savefig("horseshoe.png")
     plt.close()
+
+    
 
 
 if __name__ == "__main__":
