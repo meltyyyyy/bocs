ca8f11e7c95e0d39f5d5a4cbfbe2b976d2f3f6fb
takeru.abe <takeru.abe@gaiax.com>
2022-09-20 11:51:14 +0900

    update sblr.py

--- sblr/sblr.py
+++ sblr/sblr.py
@@ -18,8 +18,10 @@ class SparseBayesianLinearRegression:
         """
         assert X.shape[1] != self.n_vars, "The number of variables does not match."
 
-        
-
+        # X = x_1, x_2, ... , x_n
+        # ↓
+        # X = x_1, x_2, ... , x_n, x_1*x_2, x_1*x_3, ... , x_n * x_ n-1
+        X = self._order_effects(X)
 
     def _order_effects(self, X: np.ndarray) -> np.ndarray:
         """Compute order effects
@@ -53,3 +55,49 @@ class SparseBayesianLinearRegression:
             X_allpairs = np.append(X_allpairs, np.prod(x_comb, axis=2), axis=1)
 
         return X_allpairs
+
+    def bhs(X: np.ndarray, y: np.ndarray, n_samples: int = 1000, burnin: int = 200):
+        n, p = X.shape
+        XtX = X.T @ X
+
+        beta = np.zeros((p, n_samples))
+        sigma2 = 1
+        lambda2 = np.random.uniform(size=p)
+        tau2 = 1
+        nu = np.ones(p)
+        xi = 1
+
+        # Run Gibbs Sampler
+        for i in range(n_samples + burnin):
+            Lambda_star = tau2 * np.diag(lambda2)
+            A = XtX + np.linalg.inv(Lambda_star)
+            A_inv = np.linalg.inv(A)
+            b = np.random.multivariate_normal(A_inv @ X.T @ y, sigma2 * A_inv)
+
+            # Sample sigma^2
+            e = y - np.dot(X, b)
+            shape = (n + p) / 2.
+            scale = np.dot(e.T, e) / 2. + np.sum(b**2 / lambda2) / tau2 / 2.
+            sigma2 = 1. / np.random.gamma(shape, 1. / scale)
+
+            # Sample lambda^2
+            scale = 1. / nu + b**2. / 2. / tau2 / sigma2
+            lambda2 = 1. / np.random.exponential(1. / scale)
+
+            # Sample tau^2
+            shape = (p + 1.) / 2.
+            scale = 1. / xi + np.sum(b**2. / lambda2) / 2. / sigma2
+            tau2 = 1. / np.random.gamma(shape, 1. / scale)
+
+            # Sample nu
+            scale = 1. + 1. / lambda2
+            nu = 1. / np.random.exponential(1. / scale)
+
+            # Sample xi
+            scale = 1. + 1. / tau2
+            xi = 1. / np.random.exponential(1. / scale)
+
+            if i >= burnin:
+                beta[:, i - burnin] = b
+
+        return beta
