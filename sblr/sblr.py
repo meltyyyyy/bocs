e72674b8bb8ea7a992c6bf5a64ebe163e938231d
takeru.abe <takeru.abe@gaiax.com>
2022-09-20 11:41:54 +0900

    update sblr.py

--- sblr/sblr.py
+++ sblr/sblr.py
@@ -1,11 +1,13 @@
 import numpy as np
+from itertools import combinations
 
 
 class SparseBayesianLinearRegression:
     def __init__(self, n_vars: int, order: int, random_state: int = 42):
         self.n_vars = n_vars
         self.order = order
-        self.random_state = random_state
+        self.rs = np.random.RandomState(random_state)
+        self.beta = self.rs.normal(0, 1, size=n_vars)
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         """Fit Sparse Bayesian Linear Regression
@@ -18,3 +20,36 @@ class SparseBayesianLinearRegression:
 
         
 
+
+    def _order_effects(self, X: np.ndarray) -> np.ndarray:
+        """Compute order effects
+        Computes data matrix for all coupling
+        orders to be added into linear regression model.
+
+        Order is the number of combinations that needs to be taken into consideration,
+        usually set to 2.
+
+        Args:
+            X (np.ndarray): input materix of shape (n_samples, n_vars)
+
+        Returns:
+            X_allpairs (np.ndarray): all combinations of variables up to consider,
+                                     which shape is (n_samples, Σ[i=1, order] comb(n_vars, i))
+        """
+        assert X.shape[1] != self.n_vars, "The number of variables does not match."
+
+        n_samples, n_vars = X.shape
+        X_allpairs = X.copy()
+
+        for i in range(2, self.order + 1, 1):
+
+            # generate all combinations of indices (without diagonals)
+            offdProd = np.array(list(combinations(np.arange(n_vars), i)))
+
+            # generate products of input variables
+            x_comb = np.zeros((n_samples, offdProd.shape[0], i))
+            for j in range(i):
+                x_comb[:, :, j] = X[:, offdProd[:, j]]
+            X_allpairs = np.append(X_allpairs, np.prod(x_comb, axis=2), axis=1)
+
+        return X_allpairs
