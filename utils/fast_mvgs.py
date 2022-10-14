91f41013f3b6b69e391c7b5735dbc9bd376d1858
takeru.abe <takeru.abe@gaiax.com>
2022-10-14 14:13:32 +0900

    update encoders.py

--- utils/fast_mvgs.py
+++ utils/fast_mvgs.py
@@ -3,25 +3,27 @@ import numpy.typing as npt
 
 
 def fast_mvgs(Phi: npt.NDArray, alpha: npt.NDArray, D: npt.NDArray) -> npt.NDArray:
-    """Fast sampler for Multivariate Gaussian distributions
+    """
+    Fast sampler for Multivariate Gaussian distributions.
+
     Applicable for large p, p > n of
     the form N(mu, Σ), where
         mu = Σ Phi^T y
         Σ  = (Phi^T Phi + D^-1)^-1
 
-    Time complexity is O(n^2 p)
+    Time complexity is O(n^2 p).
 
     <Reference>
     Fast sampling with Gaussian scale-mixture priors in high-dimensional regression.
     https://arxiv.org/pdf/1506.04778.pdf
 
     Args:
-        Phi (np.ndarray): Matrix of shape (n, p)
-        alpha (np.ndarray): Array of shape (n, 1)
-        D (np.ndarray): Matrix of shape (p, p)
+        Phi (npt.NDArray): Matrix of shape (n, p)
+        alpha (npt.NDArray): Array of shape (n, 1)
+        D (npt.NDArray): Matrix of shape (p, p)
 
     Returns:
-        np.ndarray: Array os shape (p, 1)
+        npt.NDArray: Array os shape (p, 1)
     """
 
     n, p = Phi.shape
@@ -39,24 +41,28 @@ def fast_mvgs(Phi: npt.NDArray, alpha: npt.NDArray, D: npt.NDArray) -> npt.NDArr
 
 
 def fast_mvgs_(Phi: npt.NDArray, PtP: npt.NDArray, alpha: npt.NDArray, D: npt.NDArray) -> npt.NDArray:
-    """Fast sampler for Multivariate Gaussian distributions
+    """
+    Fast sampler for Multivariate Gaussian distributions.
 
     Applicable for small p of
     the form N(mu, Σ), where
         mu = Σ Phi' y
         Σ  = (Phi^T Phi + D^-1)^-1
 
-    Time complexity is O(n)
+    Time complexity is O(n).
 
     <Reference>
     Fast sampling of gaussian markov random fields.
     https://arxiv.org/pdf/1506.04778.pdf
 
     Args:
-        Phi (np.ndarray): Matrix of shape (n, p)
-        PtP (np.ndarray): Matrix of shape (p, p)
-        alpha (np.ndarray): Array of shape (n, 1)
-        D (np.ndarray): Matrix of shape (p, p)
+        Phi (npt.NDArray): Matrix of shape (n, p)
+        PtP (npt.NDArray): Matrix of shape (p, p)
+        alpha (npt.NDArray): Array of shape (n, 1)
+        D (npt.NDArray): Matrix of shape (p, p)
+
+    Returns:
+        npt.NDArray: Array os shape (p, 1)
     """
 
     p = Phi.shape[1]
