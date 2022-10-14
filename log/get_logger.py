1bd232a8047a2a21a7335e23d509f1467358854b
takeru.abe <takeru.abe@gaiax.com>
2022-10-14 14:56:37 +0900

    update get_logger.p

--- log/get_logger.py
+++ log/get_logger.py
@@ -1,12 +1,20 @@
 import json
 from logging import getLogger, config
+import os
 
+LOG_DIR = '/root/bocs/log/'
 LOG_CONF = '/root/bocs/log/config.json'
 
 
-def get_logger(name: str):
+def get_logger(name: str, filepath=None):
     with open(LOG_CONF, 'r') as f:
         log_conf = json.load(f)
+
+    if filepath is not None:
+        filename = os.path.splitext(os.path.basename(filepath))[0]
+        filename = LOG_DIR + filename + '.log'
+        log_conf['handlers']['fileHandler']['filename'] = filename
+
     config.dictConfig(log_conf)
     logger = getLogger(name)
     return logger
