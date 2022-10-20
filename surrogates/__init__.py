7770a8a604b06ac5d0fbf3d533e758918b97d5be
takeru.abe <takeru.abe@gaiax.com>
2022-10-20 10:20:08 +0900

    update _sblr.py

--- surrogates/__init__.py
+++ surrogates/__init__.py
@@ -1,10 +1,7 @@
 from ._sblr import SparseBayesianLinearRegression
 from ._blr import BayesianLinearRegression
-from ._fast_mvgs import fast_mvgs, fast_mvgs_
 
 __all__ = [
     'SparseBayesianLinearRegression',
     'BayesianLinearRegression',
-    'fast_mvgs',
-    'fast_mvgs_'
 ]
--- surrogates/_sblr.py
+++ surrogates/_sblr.py
@@ -2,7 +2,6 @@ from log import get_logger
 from itertools import combinations
 from typing import Tuple
 from scipy.special import comb
-from ._fast_mvgs import fast_mvgs, fast_mvgs_
 import matplotlib.pyplot as plt
 import numpy.typing as npt
 import numpy as np
@@ -172,3 +171,88 @@ class SparseBayesianLinearRegression:
                 beta[:, i - burnin] = b
 
         return beta, beta0
+
+
+def fast_mvgs(Phi: npt.NDArray, alpha: npt.NDArray, D: npt.NDArray) -> npt.NDArray:
+    """
+    Fast sampler for Multivariate Gaussian distributions.
+
+    Applicable for large p, p > n of
+    the form N(mu, Σ), where
+        mu = Σ Phi^T y
+        Σ  = (Phi^T Phi + D^-1)^-1
+
+    Time complexity is O(n^2 p).
+
+    <Reference>
+    Fast sampling with Gaussian scale-mixture priors in high-dimensional regression.
+    https://arxiv.org/pdf/1506.04778.pdf
+
+    Args:
+        Phi (npt.NDArray): Matrix of shape (n, p)
+        alpha (npt.NDArray): Array of shape (n, 1)
+        D (npt.NDArray): Matrix of shape (p, p)
+
+    Returns:
+        npt.NDArray: Array os shape (p, 1)
+    """
+
+    n, p = Phi.shape
+
+    d = np.diag(D)
+    u = np.random.randn(p) * np.sqrt(d)
+    delta = np.random.randn(n)
+    v = np.dot(Phi, u) + delta
+    mult_vector = np.vectorize(np.multiply)
+    Dpt = mult_vector(Phi.T, d[:, np.newaxis])
+    w = np.linalg.solve(np.matmul(Phi, Dpt) + np.eye(n), alpha - v)
+    x = u + np.dot(Dpt, w)
+
+    return x
+
+
+def fast_mvgs_(Phi: npt.NDArray, PtP: npt.NDArray, alpha: npt.NDArray, D: npt.NDArray) -> npt.NDArray:
+    """
+    Fast sampler for Multivariate Gaussian distributions.
+
+    Applicable for small p of
+    the form N(mu, Σ), where
+        mu = Σ Phi' y
+        Σ  = (Phi^T Phi + D^-1)^-1
+
+    Time complexity is O(n).
+
+    <Reference>
+    Fast sampling of gaussian markov random fields.
+    https://arxiv.org/pdf/1506.04778.pdf
+
+    Args:
+        Phi (npt.NDArray): Matrix of shape (n, p)
+        PtP (npt.NDArray): Matrix of shape (p, p)
+        alpha (npt.NDArray): Array of shape (n, 1)
+        D (npt.NDArray): Matrix of shape (p, p)
+
+    Returns:
+        npt.NDArray: Array os shape (p, 1)
+    """
+
+    p = Phi.shape[1]
+    D_inv = np.diag(1. / np.diag(D))
+
+    # regularize PtP + Dinv matrix for small negative eigenvalues
+    try:
+        L = np.linalg.cholesky(PtP + D_inv)
+    except BaseException:
+        M = PtP + D_inv
+        M = (M + M.T) / 2.
+        max_eig = np.max(np.linalg.eigvals(M))
+        max_eig = np.real_if_close(max_eig)
+        L = np.linalg.cholesky(M + max_eig * 1e-15 * np.eye(M.shape[0]))
+
+    v = np.linalg.solve(L, np.dot(Phi.T, alpha))
+    m = np.linalg.solve(L.T, v)
+    w = np.linalg.solve(L.T, np.random.randn(p))
+
+    x = m + w
+
+    return x
