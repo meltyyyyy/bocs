fe62f81448d9643541b92e51c1a77b90157b90ae
meltyyyyy <takeru.abe.g1@gmail.com>
2022-11-14 21:30:37 +0900

    update miqp.py

---
+++ bocs/ohe/miqp.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+import sys
+import os
+import numpy as np
+import numpy.typing as npt
+from log import get_logger
+from utils import sample_integer_matrix, encode_one_hot, decode_one_hot, get_config
+from aquisitions import simulated_annealing
+from surrogates import BayesianLinearRegressor
+from exps import find_optimum, load_study
+from dotenv import load_dotenv
+
+load_dotenv()
+config = get_config()
+logger = get_logger(__name__, __file__)
+EXP = "miqp"
+N_TRIAL = 500
+
+
+def bocs_sa_ohe(objective, low: int, high: int, n_vars: int, n_init: int = 10,
+                n_trial: int = N_TRIAL, sa_reruns: int = 5, λ: float = 10e+8):
+    # Set the number of Simulated Annealing reruns
+    sa_reruns = 5
+
+    # Initial samples
+    X = sample_integer_matrix(n_init, low, high, n_vars)
+    y = objective(X)
+
+    # Convert to one hot
+    range_vars = high - low + 1
+    X = encode_one_hot(low, high, n_vars, X)
+
+    # Define surrogate model
+    blr = BayesianLinearRegressor(range_vars * n_vars, 2)
+    blr.fit(X, y)
+
+    def penalty(x):
+        p = 0
+        for i in range(n_vars):
+            p += λ * \
+                ((1 - np.sum(x[0, i * range_vars: (i + 1) * range_vars])) ** 2)
+        return p
+
+    for i in range(n_trial):
+
+        def surrogate_model(x): return blr.predict(x) - penalty(x)
+
+        sa_X = np.zeros((sa_reruns, range_vars * n_vars))
+        sa_y = np.zeros(sa_reruns)
+
+        for j in range(sa_reruns):
+            opt_X, opt_y = simulated_annealing(
+                surrogate_model,
+                range_vars * n_vars,
+                cooling_rate=0.99,
+                n_iter=100,
+                n_flips=1)
+
+            sa_X[j, :] = opt_X[-1, :]
+            sa_y[j] = opt_y[-1]
+
+        max_idx = np.argmax(sa_y)
+        x_new = sa_X[max_idx, :]
+
+        # evaluate model objective at new evaluation point
+        x_new = np.atleast_2d(x_new)
+        y_new = objective(decode_one_hot(low, high, n_vars, x_new))
+
+        # Update posterior
+        X = np.vstack((X, x_new))
+        y = np.hstack((y, y_new))
+
+        # Update surrogate model
+        blr.fit(X, y)
+
+        # log current solution
+        x_curr = decode_one_hot(low, high, n_vars, x_new).astype(int)
+        y_curr = objective(x_curr)
+        logger.info(f'x{i}: {x_curr[0]}, y{i}: {y_curr[0]}')
+
+    X = X[n_init:, :]
+    y = y[n_init:]
+    return X, y
+
+
+def run_bayes_opt(Q: npt.NDArray,
+                  low: int, high: int):
+
+    # define objective
+    def objective(X: npt.NDArray) -> npt.NDArray:
+        return np.diag(X @ Q @ X.T)
+
+    # find global optima
+    opt_x, opt_y = find_optimum(objective, low, high, Q.shape[0])
+    logger.info(f'opt_y: {opt_y}, opt_x: {opt_x}')
+
+    _, y = bocs_sa_ohe(objective,
+                       low=low,
+                       high=high,
+                       n_vars=Q.shape[0])
+    y = np.maximum.accumulate(y)
+
+    return opt_y - y
+
+
+if __name__ == "__main__":
+    n_vars, low, high = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
+
+    # load study, extract
+    study = load_study(EXP, f'{n_vars}.json')
+    Q = study['Q']
+    n_runs = study['n_runs']
+    logger.info(f'experiment: {EXP}, n_vars: {n_vars}')
+
+    # for store
+    data = np.zeros((N_TRIAL, n_runs))
+
+    # run Bayesian Optimization
+    for i in range(n_runs):
+        logger.info(f'QUBO:\n{Q[i]}')
+        logger.info(f'############ exp{i} start ############')
+        data[:, i] = run_bayes_opt(Q[i], low, high)
+        logger.info(f'############  exp{i} end  ############')
+
+    # save
+    now = datetime.now()
+    filepath = config['output_dir'] + \
+        f'{EXP}/' + now.strftime("%m%d") + '/' + f'ohe_{n_vars}.npy'
+    dirname = os.path.dirname(filepath)
+    os.makedirs(dirname, exist_ok=True)
+    np.save(filepath, data)
